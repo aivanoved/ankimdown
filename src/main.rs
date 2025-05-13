844abb904f6c54f6fc1ebc7f85fdbedc6615d1a3
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2025-05-13 21:52:07 +0300

    wip: wip

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,10 @@
-use ankimdown::markdown::{
-    text::Text,
-    util::{log_markdown_events, log_markdown_str},
-};
-use pulldown_cmark::{Event, Options, Parser};
+use ankimdown::markdown::util::log_markdown_str;
 
 #[allow(dead_code)]
 fn markdown_ast() {
     let markdown_text: &str = r#"abc _dfg_ **hij** ~~jkl~~"#;
 
     log_markdown_str(markdown_text);
-
-    let mut events = Parser::new_ext(markdown_text, Options::all()).collect::<Vec<Event>>();
-    events = events[1..events.len() - 1].to_vec();
-
-    log_markdown_events(&mut events.clone().into_iter());
-
-    let mut events_iter = events.iter();
-
-    for _ in 0..6 {
-        println!(
-            "{}",
-            format!("{}", Text::try_from_events(&mut events_iter).unwrap())
-        )
-    }
 }
 
 #[allow(dead_code)]
@@ -33,12 +15,15 @@ fn markdown_parser() {
 # hello
 ## Meaning
 a **greeting**
+- 1
+- 2
+- 3
 "#;
 
     log_markdown_str(markdown_text);
 }
 
 fn main() {
-    // markdown_ast();
-    markdown_parser();
+    markdown_ast();
+    // markdown_parser();
 }
--- src/markdown/ast.rs
+++ src/markdown/ast.rs
@@ -1,5 +1,11 @@
+use std::slice::Iter;
+
+use pulldown_cmark::Event;
+
 use crate::markdown::heading::Heading;
 use crate::markdown::text::Text;
+
+use super::heading;
 
 #[derive(Debug, Clone)]
 pub enum Node {
@@ -11,18 +17,29 @@ pub enum Node {
         heading: Heading,
         subnodes: Vec<Node>,
     },
-    // Paragraph {
-    //     subnodes: Vec<Node>,
-    // },
-}
-
-impl std::fmt::Display for Node {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        Node::write_indented(f, self, 0)
-    }
+    Paragraph {
+        subnodes: Vec<Node>,
+    },
 }
 
 impl Node {
+    fn try_event_paragraph(events: &mut Iter<Event>) -> Result<Self, String> {
+        todo!()
+    }
+
+    pub fn try_from_events(events: &mut Iter<Event>) -> Result<Self, String> {
+        let mut nodes = vec![];
+        while events.len() > 0 {
+            if let Ok(paragraph) = Self::try_event_paragraph(events) {
+                nodes.push(paragraph);
+            } else if let Ok(heading) = Self::try_event_paragraph(events) {
+                nodes.push(heading);
+            } else {
+                return Err("Unsupported main node".to_string());
+            }
+        }
+        Ok(Self::Document { subnodes: nodes })
+    }
     fn write_indented(
         f: &mut std::fmt::Formatter<'_>,
         node: &Node,
@@ -34,6 +51,9 @@ impl Node {
                     Self::write_indented(f, subnode, level)?;
                 }
             }
+            Node::Text(text) => {
+                write!(f, "{:indent$}{}", "", text, indent = level * 2)?;
+            }
             Node::Heading { heading, subnodes } => {
                 write!(f, "{:indent$}{heading}", "", indent = level * 2)?;
                 writeln!(f)?;
@@ -41,8 +61,10 @@ impl Node {
                     Self::write_indented(f, subnode, level + 1)?;
                 }
             }
-            Node::Text(text) => {
-                write!(f, "{:indent$}{}", "", text, indent = level * 2)?;
+            Node::Paragraph { subnodes } => {
+                for node in subnodes {
+                    Self::write_indented(f, node, level)?;
+                }
             }
         }
         Ok(())
@@ -66,6 +88,9 @@ impl Node {
                     result.push(Self::tree_view(level + 1, node, idx == size - 1));
                 }
             }
+            Node::Text(text) => {
+                result.push(format!("{}Text: {}", indent(level, last), text));
+            }
             Node::Heading { heading, subnodes } => {
                 result.push(format!(
                     "{}Heading: #H{}",
@@ -87,8 +112,12 @@ impl Node {
                     result.push(Self::tree_view(level + 1, subnode, idx == size - 1));
                 }
             }
-            Node::Text(text) => {
-                result.push(format!("{}Text: {}", indent(level, last), text));
+            Node::Paragraph { subnodes } => {
+                result.push(format!("{}Paragraph", indent(level, last)));
+                let size = subnodes.len();
+                for (idx, subnode) in subnodes.iter().enumerate() {
+                    result.push(Self::tree_view(level + 1, subnode, idx == size - 1));
+                }
             }
         };
         result.join("\n")
@@ -96,6 +125,12 @@ impl Node {
 
     pub fn tree_to_string(&self) -> String {
         Self::tree_view(0, self, true)
+    }
+}
+
+impl std::fmt::Display for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Node::write_indented(f, self, 0)
     }
 }
 
