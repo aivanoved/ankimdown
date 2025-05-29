2d89cc107494d64cba19137acc6838b1676e6f83
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2025-05-29 14:20:45 +0300

    wip: wip

--- src/information.rs
+++ src/information.rs
@@ -1,7 +1,35 @@
+use crate::ankigen::db_model::card::Card;
+use crate::markdown::ast::Node;
+
+type Example = Vec<String>;
+
 #[derive(Debug, Clone, Default)]
-pub struct SimpleInformation {
+pub struct InformationContent {
     pub word: String,
     pub definitions: Vec<String>,
+    pub examples: Vec<Option<Example>>,
+}
+
+pub trait IntoCards {
+    fn into_cards(
+        &self,
+        information_content: &InformationContent,
+    ) -> Vec<Card>;
+}
+
+pub struct Information {
+    pub information_type: Box<dyn IntoCards>,
+    pub content: InformationContent,
+}
+
+impl Information {
+    pub fn from_ast(node: &Node) -> Self {
+        todo!()
+    }
+
+    pub fn get_cards(&self) -> Vec<Card> {
+        self.information_type.into_cards(&self.content)
+    }
 }
 
 #[cfg(test)]
@@ -9,10 +37,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_simple_information() {
-        let simple_info = SimpleInformation {
+    fn test_information() {
+        let simple_info = InformationContent {
             word: "hello".to_string(),
             definitions: vec!["a greeting".to_string()],
+            examples: vec![None],
         };
 
         assert_eq!(simple_info.word, "hello");
